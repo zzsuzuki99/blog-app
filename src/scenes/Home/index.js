ec96bcebb7db157856d2a464c38e350245cc0a10
Steven <nguyen.quocnam10a1@gmail.com>
2018-08-11 07:24:21 +0700

    init project

---
+++ src/scenes/Home/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
