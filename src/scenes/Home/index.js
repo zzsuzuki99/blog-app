27fc8e5dc881b85f26f82d522de7889266b8094a
Steven <nguyen.quocnam10a1@gmail.com>
2018-08-28 22:27:52 +0700

    update project

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-
+import React, { Component } from 'react'
+import logo from '../../logo.svg'
+import NavBarComponent from '../../components/Nav'
 class Home extends Component {
-  render() {
+  render () {
     return (
       <div className="Home">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        
+        <nav>
+          <NavBarComponent />
+        </nav>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default Home
